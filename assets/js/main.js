e243e931919b928a038f6d145b35368ae9ac7780
Andrew Kamm <ak@saturn2.local>
2015-12-04 15:56:32 -0600

    Adjust enqueue, include main.js

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+/**
+ * Primary JS file for strtr.
+ *
+ * Localized vars available as strtrThemeVars.
+ */
+
+(function($) {
+
+	var strtrTheme = {};
+
+	$(document).ready(function() {
+		// ...
+	});
+
+})(jQuery);
--- functions.php
+++ functions.php
@@ -95,9 +95,33 @@ add_action( 'widgets_init', 'strtr_widgets_init' );
  * Enqueue scripts and styles.
  */
 function strtr_scripts() {
+
+	$assets_url = get_stylesheet_directory_uri() . '/assets/';
+
 	wp_enqueue_style( 'strtr-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'strtr-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script(
+		'strtr-skip-link-focus-fix',
+		$assets_url . 'js/skip-link-focus-fix.js',
+		array(),
+		date( "YM" ),
+		true
+	);
+
+	wp_register_script(
+		'strtr-main',
+		$assets_url . '/js/main.js',
+		array( 'jquery' ),
+		date( "YM" ),
+		true
+	);
+
+	// "Localized" vars to be used by our main JS file.
+	$vars = array(
+		'ajaxUrl' => admin_url( 'admin-ajax.php' ),
+	);
+	wp_localize_script( 'strtr-main', 'strtrThemeVars', $vars );
+	wp_enqueue_script( 'strtr-main' );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
