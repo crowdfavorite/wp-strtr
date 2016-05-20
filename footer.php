fc42a0ce9b9bbfdcff3bc0d47f8d62e5b9d38202
Andrew Kamm <ak@kamm.co>
2016-05-20 16:59:49 -0500

    Normalize classes in footer

--- footer.php
+++ footer.php
@@ -2,25 +2,22 @@
 /**
  * The template for displaying the footer.
  *
- * Contains the closing of the #content div and all content after
- *
  * @package strtr
  */
 
 ?>
-		</div><!-- /.inner.for-content -->
-	</div><!-- #content -->
+		</div><!-- /.inner.for-site-content -->
+	</div><!-- /.site-content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="inner for-colophon">
+		<div class="inner for-site-footer">
 
 			<div class="site-info">
-
-				<div><a href="<?php echo esc_url( __( 'https://wordpress.org/', 'strtr' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'strtr' ), 'WordPress' ); ?></a></div>
-
+				<!-- ... -->
 			</div><!-- .site-info -->
-		</div><!-- /.inner.for-colophon -->
-	</footer><!-- #colophon -->
+
+		</div><!-- /.inner.for-site-footer -->
+	</footer><!-- /.site-footer -->
 
 </div><!-- #page -->
 
